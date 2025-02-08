0140aaf83c24daf88687523a135fd8e4e4b3dcbc
D-Seagull <dchaika.work@gmail.com>
2025-02-08 19:40:35 +0100

    added button to not found

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,15 @@
+import { useRef } from "react";
+import { Link, useLocation } from "react-router-dom";
+
 const NotFoundPage = () => {
-  return <p>Not found 404 </p>;
+  const location = useLocation();
+  const gobackURL = useRef(location?.state ?? "/");
+  return (
+    <div>
+      <Link to={gobackURL.current}>Go Back</Link>
+      <p>Not found 404 </p>;
+    </div>
+  );
 };
 
 export default NotFoundPage;
